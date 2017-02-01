b002973a88b0a1d2e57cdcd1167cf00f6b5b9eba
Roman Mykitchak <deviphone@i.ua>
2017-02-01 14:51:31 +0200

    init commit

---
+++ teamvoy/teamvoy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  teamvoy
+//
+//  Created by Roman Mykitchak on 2/1/17.
+//  Copyright © 2017 Roman Mykitchak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
